6993e4baf044d94e7a20dc1ae8902d2200744950
Colby Allen <colbyallen012@gmail.com>
2020-01-15 10:22:16 -0700

    new quote button function working with console log

--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 export class QuoteCard extends Component {
-  
+
+  newQuote = async (e) => {
+    e.preventDefault()
+    console.log('hazah')
+  }
+
   render() {
     return (
       <div className={QuoteCard}>
         <h2>Yeezy Card</h2>
         <p>{this.props.quote}</p>
+        <form onSubmit={this.newQuote}>
+          <button>New Quote</button>
+        </form>
       </div>
     )
   }
