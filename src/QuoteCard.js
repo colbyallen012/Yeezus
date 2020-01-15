97ccc4bd71815bbd9037927bda59f96e8c85d823
Colby Allen <colbyallen012@gmail.com>
2020-01-15 11:44:02 -0700

    rearrange image and card

--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -12,12 +12,10 @@ export class QuoteCard extends Component {
 
   render() {
     return (
-      <div className={QuoteCard}>
-        <img className='pablo' src={pablo} alt='kanye-west'/>
-        <h2>Ye</h2>
-        <h3>@kanyewest</h3>
-        <h4>{this.props.today}</h4>
+      <div className='QuoteCard'>
+        <h2>{this.props.today}</h2> 
         <p className='quote'>{this.props.quote}</p>
+        <img className='pablo' src={pablo} alt='kanye-west'/>
         <form onSubmit={this.newQuote}>
           <button className='quote-button'>New Quote</button>
         </form>
