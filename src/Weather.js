b36cf06aace62b4a2bd8f54468b6ac742a450ca6
Colby Allen <colbyallen012@gmail.com>
2020-02-11 10:30:58 -0700

    add logic for weather icons, refactor later

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,9 @@ import clearDay from './Utils/clear-day.png'
 import clearNight from './Utils/clear-night.png'
 import rain from './Utils/rain.png'
 import snow from './Utils/snow.png'
-import wind from './Utils/wind.png'
+import wind from './Utils/windy.png'
 import fog from './Utils/fog.png'
 import cloudy from './Utils/cloudy.png'
-import fog from './Utils/clear-night.png'
 import partlyCloudyDay from './Utils/partly-cloudy-day.png'
 import partlyCloudyNight from './Utils/partly-cloudy-night.png'
 
@@ -19,8 +18,18 @@ export class Weather extends Component {
       return <img src={clearNight} alt='weather'/>
     } else if (this.props.icon === 'rain') {
       return <img src={rain} alt='weather'/>
+    } else if (this.props.icon === 'snow') {
+      return <img src={snow} alt='weather'/>
+    } else if (this.props.icon === 'wind') {
+      return <img src={wind} alt='weather'/>
     } else if (this.props.icon === 'fog') {
       return <img src={fog} alt='weather'/>
+    } else if (this.props.icon === 'cloudy') {
+      return <img src={cloudy} alt='weather'/>
+    } else if (this.props.icon === 'partly-cloudy-day') {
+      return <img src={partlyCloudyDay} alt='weather'/>
+    } else if (this.props.icon === 'partly-cloudy-night') {
+      return <img src={partlyCloudyNight} alt='weather'/>
     } else {
       return null
     }
